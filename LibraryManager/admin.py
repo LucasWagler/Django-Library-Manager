89902832254ac15bb2106f7aa76263a0fa890046
Lucas Wagler <lucas@wagler.dev>
2020-11-18 19:45:21 -0500

    initial testing

--- LibraryManager/admin.py
+++ LibraryManager/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Author, Book
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ['name', 'country']
+    list_display = ('name', 'country')
+    search_fields = ['name', 'country']
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'num_pages')
+    search_fields = ['title', 'author__name']
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
--- LibraryManager/models.py
+++ LibraryManager/models.py
@@ -1,49 +1,119 @@
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):
 
-    title = models.CharField(_("Title"), max_length=100)
-    num_pages = models.IntegerField(_("Number of Pages"))
-
-
-    class Meta:
-        verbose_name = _("book")
-        verbose_name_plural = _("books")
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("book_detail", kwargs={"pk": self.pk})
 
 class Author(models.Model):
 
-    name = models.CharField(_("Name"), max_length=50)
-    country = models.CharField(_("Country"), max_length=50)
+    name = models.CharField(max_length=50, default="Unknown")
+    country = models.CharField(max_length=50, default="Unknown", blank=True)
 
-    class Meta:
-        verbose_name = _("author")
-        verbose_name_plural = _("authors")
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("author_detail", kwargs={"pk": self.pk})
-
-class Employee(models.Model):
-
-    name = models.CharField(_("Name"), max_length=50)
-    email = models.EmailField(_("Email"), max_length=254)
-    phone = models.PhoneNumberField(_("Phone Number"))
-
-    class Meta:
-        verbose_name = _("employee")
-        verbose_name_plural = _("employees")
+    # class Meta:
+    #     verbose_name = _("author")
+    #     verbose_name_plural = _("authors")
 
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse("employee_detail", kwargs={"pk": self.pk})
+    # def get_absolute_url(self):
+    #     return reverse("author_detail", kwargs={"pk": self.pk})
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey("LibraryManager.Author", on_delete=models.CASCADE)
+    num_pages = models.IntegerField()
+
+
+
+    # class Meta:
+    #     verbose_name = _("book")
+    #     verbose_name_plural = _("books")
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse("book_detail", kwargs={"pk": self.pk})
+
+# class BookAuthor(models.Model):
+
+#     book = models.ForeignKey("LibraryManager.Book", verbose_name=_(""), on_delete=models.CASCADE)
+#     author = models.ForeignKey("LibraryManager.Author", verbose_name=_(""), on_delete=models.CASCADE)
+
+#     class Meta:
+#         verbose_name = _("bookauthor")
+#         verbose_name_plural = _("bookauthors")
+
+#     def __str__(self):
+#         return self.name
+
+#     def get_absolute_url(self):
+#         return reverse("bookauthor_detail", kwargs={"pk": self.pk})
+
+# class Branch(models.Model):
+
+#     name = models.CharField(_("Name"), max_length=50)
+#     city = models.CharField(_("City"), max_length=50)
+
+#     class Meta:
+#         verbose_name = _("branch")
+#         verbose_name_plural = _("branchs")
+
+#     def __str__(self):
+#         return self.name
+
+#     def get_absolute_url(self):
+#         return reverse("branch_detail", kwargs={"pk": self.pk})
+
+
+# class BookBranchCopies(models.Model):
+
+#     book = models.ForeignKey("LibraryManager.Book", verbose_name=_(""), on_delete=models.CASCADE)
+#     branch = models.ForeignKey("LibraryManager.Branch", verbose_name=_(""), on_delete=models.CASCADE)
+#     num_copies = models.IntegerField(_("Number of Copies"))
+
+#     class Meta:
+#         verbose_name = _("bookcopies")
+#         verbose_name_plural = _("bookcopiess")
+
+#     def __str__(self):
+#         return self.name
+
+#     def get_absolute_url(self):
+#         return reverse("bookcopies_detail", kwargs={"pk": self.pk})
+
+
+# class Employee(models.Model):
+
+#     name = models.CharField(_("Name"), max_length=50)
+#     email = models.EmailField(_("Email"), max_length=254)
+#     phone = models.PhoneNumberField(_("Phone Number"))
+#     salary = models.FloatField(_("Salary"))
+#     branch = models.ForeignKey("LibraryManager.Branch", verbose_name=_(""), on_delete=models.SET_NULL)
+#     department = models.ForeignKey("LibraryManager.Department", verbose_name=_(""), on_delete=models.SET_NULL)
+
+#     class Meta:
+#         verbose_name = _("employee")
+#         verbose_name_plural = _("employees")
+
+#     def __str__(self):
+#         return self.name
+
+#     def get_absolute_url(self):
+#         return reverse("employee_detail", kwargs={"pk": self.pk})
+
+# class Department(models.Model):
+
+#     name = models.CharField(_("Name"), max_length=50)
+#     budget = models.FloatField(_("Budget"))
+
+#     class Meta:
+#         verbose_name = _("department")
+#         verbose_name_plural = _("departments")
+
+#     def __str__(self):
+#         return self.name
+
+#     def get_absolute_url(self):
+#         return reverse("department_detail", kwargs={"pk": self.pk})
